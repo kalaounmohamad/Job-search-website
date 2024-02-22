e02b6f163997ac4c1580814299ad814cdffc820c
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-22 13:53:12 +0200

    Navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,180 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../img/logo.png";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+// import FaTimes from "./x-icon.svg";
+// import FiMenu from "./menu-icon.svg";
+
+export default function Navbar() {
+  const [menuSet, setMenuSet] = useState(false);
+  const pathname = usePathname();
+
+  const toggleMenu = () => {
+    setMenuSet(!menuSet);
+  };
+
+  const closeMenu = () => {
+    setMenuSet(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setMenuSet(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div>
+      <nav className="max-w-screen-2xl mx-auto flex justify-between items-center mt-4 sm:mt-8 flex-wrap w-full">
+        <div className="flex gap-7 md:gap-10 lg:gap-14 text-sm lg:text-base mx-auto md:mx-0">
+          <Link href="/" className="w-[120px] lg:w-auto ">
+            <Image
+              src={Logo}
+              alt="Jobnow logo"
+              width={160}
+              loading="eager"
+              className="hover:opacity-75"
+            />
+          </Link>
+
+          <div
+            className={`${
+              menuSet
+                ? " absolute bg-white md:relative top-0 z-10 pt-5 sm:pt-9  h-screen"
+                : "hidden"
+            } w-full  px-4 lg:px-0 -ml-4 md:ml-0  md:flex md:items-center md:w-auto`}
+          >
+            <ul
+              className="md:flex  
+             md:justify-between gap-6 lg:gap-12 space-y-4 md:space-y-0"
+            >
+              <li>
+                <Link
+                  href="/"
+                  onClick={closeMenu}
+                  className={`block font-bold  ${
+                    pathname === "/" ? "underline" : ""
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  href="/job"
+                  onClick={closeMenu}
+                  className={`block font-bold  ${
+                    pathname === "/projects" ? "underline" : ""
+                  }`}
+                >
+                  Job
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/aboutus"
+                  onClick={closeMenu}
+                  className={`block font-bold  ${
+                    pathname === "/projects" ? "underline" : ""
+                  }`}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  onClick={closeMenu}
+                  className={`block font-bold  ${
+                    pathname === "/tickets" ? "underline" : ""
+                  }`}
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className="md:hidden">
+                <Link
+                  href="/signin"
+                  onClick={closeMenu}
+                  className={`block font-bold   ${
+                    pathname === "/projects" ? "underline" : ""
+                  }`}
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li className="md:hidden">
+                <Link
+                  href="/createaccount"
+                  onClick={closeMenu}
+                  className={`font-bold text-black flex gap-2 items-center ${
+                    pathname === "/createaccount" ? "underline" : ""
+                  }`}
+                >
+                  <span className="flex gap-2 bg-gray-600 px-3 py-2 rounded-xl">
+                    <img
+                      src="/assets/sign_up.svg"
+                      alt="Sign up SVG"
+                      className="transform -rotate-90"
+                    />
+                    Create Account
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="hidden md:flex items-center gap-6 lg:gap-12 text-sm lg:text-base">
+          <Link
+            href="/signin"
+            onClick={closeMenu}
+            className={`block font-bold   ${
+              pathname === "/projects" ? "underline" : ""
+            }`}
+          >
+            Sign in
+          </Link>
+          <Link
+            href="/createaccount"
+            onClick={closeMenu}
+            className={` font-bold text-black flex gap-2 bg-gray-600 px-3 py-2 rounded-xl  ${
+              pathname === "/projects" ? "underline" : ""
+            }`}
+          >
+            <img
+              src="/assets/sign_up.svg"
+              alt="Sign up SVG"
+              className="transform -rotate-90"
+            />
+            Create Account
+          </Link>
+        </div>
+      </nav>
+      {menuSet ? (
+        <button
+          className="md:hidden absolute top-3 sm:top-7 right-5 text-black cursor-pointer  z-20  text-3xl p-0 font-bold"
+          onClick={toggleMenu}
+        >
+          ✕
+        </button>
+      ) : (
+        <button
+          className="md:hidden absolute top-3 sm:top-7 right-5 text-black cursor-pointer text-3xl p-0 font-bold"
+          onClick={toggleMenu}
+        >
+          ☰
+        </button>
+      )}
+    </div>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " flex flex-col min-h-screen"}>
+        <Navbar />
+        <div className="flex-grow ">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
