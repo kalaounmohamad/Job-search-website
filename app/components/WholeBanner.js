368ad2926cbf0ac0bd260c14dac7195fb0690ca6
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-27 11:27:44 +0200

    Background

--- app/components/WholeBanner.js
+++ app/components/WholeBanner.js
@@ -7,7 +7,7 @@ export default function WholeBanner() {
       <div className="relative">
         {/* Bg on += md screens */}
         <div className="hidden md:block md:absolute md:bottom-0 md:left-0 md:-z-10 md:w-screen md:h-[1000px] md:overflow-hidden">
-          <div className="hidden md:block md:absolute md:bottom-0 md:left-0 md:w-full md:h-full md:bg-gray-200  " />
+          <div className="hidden md:block md:absolute md:bottom-0 md:left-0 md:w-full md:h-full md:bg-[#F2F2F2]  " />
         </div>
 
         <Banner />
--- app/page.js
+++ app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <>
       <WholeBanner />
-      <div className="px-8 md:px-20 pt-20 bg-gray-100">
+      <div className="px-8 md:px-20 pt-20 bg-[#F6F6F6]">
         <HowItWorks />
       </div>
-      <div className="px-8 md:px-20 pt-20 bg-gray-200">
+      <div className="px-8 md:px-20 pt-20 bg-[#F2F2F2]">
         <JobCategories />
       </div>
       <div className="mx-8 md:mx-20">home</div>
