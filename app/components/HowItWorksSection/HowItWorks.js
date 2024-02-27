d3b8abda83f6fccb0431528ec01ac44b34536c29
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-27 10:16:29 +0200

    Fixed padding

--- app/components/HowItWorksSection/HowItWorks.js
+++ app/components/HowItWorksSection/HowItWorks.js
@@ -4,7 +4,7 @@ import Help from "./Help";
 
 export default function HowItWorks() {
   return (
-    <div className="max-w-screen-2xl mx-auto">
+    <div className="max-w-screen-2xl mx-auto w-full  2xl:px-20">
       <h1 className="text-4xl">How it works</h1>
       <LearnMore />
       <Help />
