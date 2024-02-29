492de00af128eb1d8e2590053ea26e27c0e8fb5a
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-29 15:19:14 +0200

    Changed structure

--- app/components/Feedback/Feedback.js
+++ app/components/Feedback/Feedback.js
@@ -49,10 +49,11 @@ export default function Feedback() {
     },
   ];
   return (
-    <div>
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-6">
+    <div className="max-w-screen-2xl mx-auto w-full 2xl:px-20">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-6 text-center lg:text-left ">
         Feedback about JobNow!
       </h1>
+
       <Swiper
         breakpoints={{
           1: {
@@ -74,38 +75,36 @@ export default function Feedback() {
       >
         {feedbackData.map((item) => (
           <SwiperSlide key={item.title} className="mr-20">
-            <div className="feedback flex flex-col md:flex-row gap-24 md:gap-16  md:items-center ">
-              <div className="texts md:w-[60%] flex flex-col items-center text-center md:text-left md:items-start">
-                <div className="1and2 flex flex-col gap-14">
-                  <p className="text-gray-400 text-xl">{item.subtitle}</p>
-                  <p className="font-medium text-xl sm:text-2xl mb-12">
-                    {item.feedback}
-                  </p>
-                </div>
-                <div className="2and3and4 ">
-                  <p className="font-semibold text-2xl mb-2">{item.author}</p>
-                  <p className="text-gray-500 text-md font-medium mb-6">
-                    {item.position}
-                  </p>
-                  <div className="flex gap-6 ml-1 justify-center md:justify-normal">
-                    <div className="swiper-button-prev bg-primary p-2 rounded-full hover:opacity-50 cursor-pointer">
-                      <RxArrowLeft
-                        className=" block text-white rounded-xl text-3xl font-bold "
-                        style={{ strokeWidth: 1 }}
-                      />
-                    </div>
-                    <div className="swiper-button-next bg-primary p-2 rounded-full hover:opacity-50 cursor-pointer">
-                      <RxArrowRight
-                        className=" block text-white rounded-xl text-3xl font-bold "
-                        style={{ strokeWidth: 1 }}
-                      />
-                    </div>
-                  </div>
+            <div class="grid  lg:grid-cols-[12fr,7fr] ">
+              <div className="1 flex flex-col text-center lg:text-left gap-14 lg:pr-12">
+                <p className="text-gray-400 text-xl">{item.subtitle}</p>
+                <p className="font-medium text-xl sm:text-2xl mb-12">
+                  {item.feedback}
+                </p>
+              </div>
+              <div className="2 picture pt-20 pb-14 lg:pt-0 lg:pb-0 w-2/3 mx-auto lg:mx-0 lg:w-auto lg:row-span-2 lg:my-auto ">
+                <div className="bg-primary h-[350px] lg:h-[500px] rounded-3xl mx-auto">
+                  <p className="text-center">{item.img}</p>
                 </div>
               </div>
-              <div className="picture md:w-[40%]">
-                <div className="bg-primary h-[500px] rounded-3xl mx-auto">
-                  <p className="text-center">{item.img}</p>
+              <div className="3 lg:pr-12 text-center lg:text-left">
+                <p className="font-semibold text-2xl mb-2">{item.author}</p>
+                <p className="text-gray-500 text-md font-medium mb-6">
+                  {item.position}
+                </p>
+                <div className="flex gap-6 ml-1 justify-center lg:justify-normal">
+                  <div className="swiper-button-prev bg-primary p-2 rounded-full hover:opacity-50 cursor-pointer">
+                    <RxArrowLeft
+                      className=" block text-white rounded-xl text-3xl font-bold "
+                      style={{ strokeWidth: 1 }}
+                    />
+                  </div>
+                  <div className="swiper-button-next bg-primary p-2 rounded-full hover:opacity-50 cursor-pointer">
+                    <RxArrowRight
+                      className=" block text-white rounded-xl text-3xl font-bold "
+                      style={{ strokeWidth: 1 }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
